04d9f5a30669fc862b29107a8aef0ab5cf916660
samfanahi <sam.fan@ahi.tech>
2023-01-25 16:32:21 +0800

    enable Eureka

--- messages/src/main/java/com/samservice/message/MessageApplication.java
+++ messages/src/main/java/com/samservice/message/MessageApplication.java
@@ -2,6 +2,7 @@ package com.samservice.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.PropertySources;
 @PropertySources({
         @PropertySource("classpath:clients-${spring.profiles.active}.properties")
 })
+@EnableEurekaClient
 public class MessageApplication {
     public static void main(String[] args) {
         SpringApplication.run(MessageApplication.class, args);
--- notifications/src/main/java/com/sam/notification/NotificationApplication.java
+++ notifications/src/main/java/com/sam/notification/NotificationApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
@@ -13,7 +14,7 @@ import org.springframework.context.annotation.PropertySources;
 @SpringBootApplication(
         scanBasePackages = {"com.sam.amqp", "com.sam.notification"}
 )
-
+@EnableEurekaClient
 @EnableFeignClients(basePackages = "com.sam.clients")
 @PropertySources({
         @PropertySource("classpath:clients-${spring.profiles.active}.properties")
